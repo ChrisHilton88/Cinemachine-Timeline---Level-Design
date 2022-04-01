022760921586465a09fc3652b58fc5839d626fc4
Chris Hilton <christopherhilton88@gmail.com>
2022-04-01 15:00:13 +0200

    V1.01 - Added cockpit and cockpit camera (POV) for the player. Also locked the players view on this camera. Added in a cockpit floor. Added a point light in the cockpit to help see. Need to make changes to this later on.

---
+++ Assets/Scripts/Environment/ISSMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ISSMovement : MonoBehaviour
+{
+    float speed = 0.5f;
+    float rotSpeed = 0.5f;
+
+
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        transform.Rotate(0, -rotSpeed * Time.deltaTime, 0, Space.World);
+    }
+}
