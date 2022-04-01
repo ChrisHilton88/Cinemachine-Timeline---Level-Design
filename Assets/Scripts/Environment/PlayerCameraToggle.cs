2222d3c56ac953abaef545b1103383d39cef17e7
Chris Hilton <christopherhilton88@gmail.com>
2022-04-01 16:37:30 +0200

    V1.02 - Implemented the camera toggle switch for the player using the R key and switching between the cockpit and 3rd person camera view

---
+++ Assets/Scripts/Environment/PlayerCameraToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class PlayerCameraToggle : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _vCams;
+
+    private int _currentVCam;
+
+
+    public void SetPriorítyCamera(int currentCam)
+    {
+        if (_vCams[currentCam].GetComponent<CinemachineVirtualCamera>())
+        {
+            _vCams[currentCam].GetComponent<CinemachineVirtualCamera>().m_Priority = 15;
+        }
+    }
+
+    public void ResetCams()
+    {
+        for (int i = 0; i < _vCams.Length; i++)
+        {
+            if (_vCams[i].GetComponent<CinemachineVirtualCamera>())
+            {
+                _vCams[i].GetComponent<CinemachineVirtualCamera>().m_Priority = 10;
+                Debug.Log("Successfully reset VCams priorities back to 10");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private int _currentCamera = 1;
+
+    private bool isCinematicPlaying;
+
+    private PlayerCameraToggle _cameraToggle;
+
+
+
+    void Start()
+    {
+        _cameraToggle = GameObject.Find("Camera_Manager").GetComponent<PlayerCameraToggle>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !isCinematicPlaying)
+        {
+            if (_currentCamera == 1)
+            {
+                _currentCamera = 0;
+            }
+            else if (_currentCamera == 0)
+            {
+                _currentCamera = 1;
+            }
+
+            _cameraToggle.ResetCams();
+            _cameraToggle.SetPriorítyCamera(_currentCamera);
+        }
+    }
+
+    void Movement()
+    {
+
+    }
+}
