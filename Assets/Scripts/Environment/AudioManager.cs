23b42d4e9631259dfc30c2e1a000386e876d4773
Chris Hilton <christopherhilton88@gmail.com>
2022-04-22 15:11:06 +0200

    V1.05 - Removed the shooting stars animations from the intro as I have having continual problems with them deactivating randomly in the Timeline. Completed half of the final cutscene including virtual camera setups, background music, sound effects, animators for the player and enemy space ship. Alos built the Main Menu with 3 buttons - Play, Credits, Quit. Built a second image box for the Credits window that comes up. Created code to deactivate the 3 buttons when the Credits button is pressed. Created code to re-activate when closing down the Credits window. Used On Click Buttons for this.

---
+++ Assets/Scripts/Environment/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [SerializeField] private AudioClip clickButtonclip;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ClickButton()
+    {
+        audioSource.clip = clickButtonclip;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Environment/Game_Manager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour
+{
+    public void QuitProgram()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
